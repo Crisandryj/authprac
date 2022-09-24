131517a5b2af45135ba62aba3afa9893819e4909
crisandryj <crisandryj@gmail.com>
2022-09-24 16:43:38 -0400

    created sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
   resources :users
 end
