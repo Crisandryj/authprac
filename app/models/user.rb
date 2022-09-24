643dbc93af26413216ee2247cd368d4745c63a1d
crisandryj <crisandryj@gmail.com>
2022-09-24 15:57:10 -0400

    changed user to 'users'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password :validations => false
-  # validates_confirmation_of :password_digest
-  # validates_presence_of :password_digest, :on => :create
+  validates_confirmation_of :password_digest
+  validates_presence_of :password_digest, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
 end
