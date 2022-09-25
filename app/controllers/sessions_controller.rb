42f88257cc3c71e1bbc563be4b20b316438cbf1a
crisandryj <crisandryj@gmail.com>
2022-09-25 16:48:44 -0400

    created form_for form for sessions:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,27 +2,27 @@ class SessionsController < ApplicationController
   def new
   end
 
-def create
-  user = User.authenticate(user_params)
-  if user.present? && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to root_url, :notice => "Logged in!"
-  else
-    flash.now.alert = "Invalid email or password"
-    render "new"
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if use
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
   end
-end
 
-def destroy
-  # deletes user session
-  session[:user_id] = nil
-  redirect_to root_path, notice: 'Logged Out'
-end
+  def destroy
+    # deletes user session
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged Out'
+  end
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(:email, :password)
-end
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 
-end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   validates_presence_of :password_digest, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret (password)
+      user
+    else
+      nil
+    end
+  end
+
 end
