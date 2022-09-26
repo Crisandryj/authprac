3a1c75c0ca0442cdbca1921bd272384c297c69f8
crisandryj <crisandryj@gmail.com>
2022-09-26 18:21:01 -0400

    updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,27 +2,29 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    user = User.authenticate(params[:email], params[:password])
-    if use
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
-    else
-      flash.now.alert = "Invalid email or password"
-      render "new"
-    end
+def create
+  user = User.authenticate(user_params)
+  if user
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Logged in!"
+  else
+    flash.now.alert = "Invalid email or password"
+    render "new"
   end
+end
 
-  def destroy
-    # deletes user session
-    session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged Out'
-  end
+def destroy
+  # deletes user session
+  session[:user_id] = nil
+  redirect_to root_path, notice: 'Logged Out'
+end
 
-  private
+private
 
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
+def user_params
+  params.require(:user).permit(:email, :password)
+end
 
-  end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,5 @@ class User < ApplicationRecord
   validates_presence_of :email
   validates_uniqueness_of :email
 
-  def self.authenticate(email, password)
-    user = find_by_email(email)
-    if user && user.password_hash == BCrypt::Engine.hash_secret (password)
-      user
-    else
-      nil
-    end
-  end
 
 end
